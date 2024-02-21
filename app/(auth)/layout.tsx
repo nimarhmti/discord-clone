63efd4f016f646f9f6e0f623addcc5880d614c44
nimarhmti <nimarahmati2000@gmail.com>
2024-02-21 10:58:13 +0330

    authentication is done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,5 +5,10 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-red-300 h-full"> {children}</div>;
+  return (
+    <div className="bg-appBase h-full flex items-center justify-center flex-col gap-y-6">
+      <h1 className="text-white text-2xl  font-bold">Discord</h1>
+      {children}
+    </div>
+  );
 }
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-const state: boolean = true;
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="">
-      <Button className={cn(state && "bg-red-300")}>click</Button>
+    <main className="bg-appBase  h-full">
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
